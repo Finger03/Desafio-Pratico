f20a6073daa2d2d7b56d7aa86ac7f30ed0f96510
Pedro Bandeira <finger.contato@gmail.com>
2024-09-13 17:10:23 -0300

    primeiro commit

---
+++ Lista-Produtos.py
@@ -0,0 +1,30 @@
+# Implemente um programa em python que faça a analise de vendas de uma lista de produtos.
+# Cada produto deverá ser um dicionario contendo o nome do produto, o preço unitario do produto e a quantidade vendida
+
+produtos = [
+    {
+        "nome": "Arroz",
+        "preco_unitario": 4.50,
+        "quantidade_vendida": 20
+    },
+    {
+        "nome": "Feijão",
+        "preco_unitario": 6.00,
+        "quantidade_vendida": 15
+    },
+    {
+        "nome": "Açúcar",
+        "preco_unitario": 3.00,
+        "quantidade_vendida": 10
+    },
+    {
+        "nome": "Óleo",
+        "preco_unitario": 5.20,
+        "quantidade_vendida": 8
+    },
+    {
+        "nome": "Macarrão",
+        "preco_unitario": 2.50,
+        "quantidade_vendida": 25
+    }
+]
