ecfb900a066a4f92de754c0596322e78a1f926d2
Pedro Bandeira <finger.contato@gmail.com>
2024-09-14 00:21:46 -0300

    correção ortografica no texto

--- Lista_Produtos.py
+++ Lista_Produtos.py
@@ -1,6 +1,6 @@
 # Implemente um programa em python que faça a analise de vendas de uma lista de produtos.
 # Cada produto deverá ser um dicionario contendo o nome do produto, o preço unitario do produto e a quantidade vendida
-# *Em um arquivo deve ser colocado o toral de produtos vendidos (quantidade), a media da quantidade de produtos vendidos e o valor toral de vendas de todos os produtos.
+# *Em um arquivo deve ser colocado o total de produtos vendidos (quantidade), a media da quantidade de produtos vendidos e o valor toral de vendas de todos os produtos.
 # *Em outro arquivo deve conter a identificação do produto mais vendido e do menos vendido com base na quantidade.
 
 
